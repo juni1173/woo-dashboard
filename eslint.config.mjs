bdd42db3dc72a367e57e48cf66a98aed0938c619
juni1173 <junaid1173@gmail.com>
2025-03-07 04:57:23 +0500

    update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,30 @@
 import js from "@eslint/js";
-import next from "@next/eslint-plugin-next";
+import next from "eslint-plugin-next"; // corrected import
 import tseslint from "@typescript-eslint/eslint-plugin";
 import tsparser from "@typescript-eslint/parser";
 
 export default [
   js.configs.recommended,
-  next.configs.recommended,
   {
+    ...next.configs.recommended,
+    rules: {
+      ...next.configs.recommended.rules,
+      "@next/next/no-img-element": "warn"
+    }
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"], //added files section.
     languageOptions: {
       parser: tsparser,
       sourceType: "module",
+    },
+    plugins: {
+        "@typescript-eslint": tseslint,
     },
     rules: {
       "react-hooks/exhaustive-deps": "error",
       "@typescript-eslint/no-unused-vars": "error",
       "@typescript-eslint/no-explicit-any": "error",
-      "@next/next/no-img-element": "warn",
     },
   },
 ];
